5a3b4620ccbbb547ad5e85c22d49db6ac4e92261
m990 <League@AdministorsiMac.attlocal.net>
2018-07-31 12:01:21 -0700

    Tasty Tomato, Pizza, Zombie Eyes, Rocket Ship Blastoff, Bananas 4eva, Banana Split, My Ages

--- src/ifs_ints_and_loops/MyAges.java
+++ src/ifs_ints_and_loops/MyAges.java
@@ -1,5 +1,10 @@
 package ifs_ints_and_loops;
 
 public class MyAges {
-
+	public static void main(String[] args) {
+		for (int i = 0; i < 15; i++) {
+			System.out.println("Once upon a time I was " + i);
+		}
+	}
 }
+
